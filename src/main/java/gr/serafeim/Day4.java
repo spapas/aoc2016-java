068a00147db81364beb412bcc0ef8acf02ebb816
Serafeim Papastefanos <spapas@gmail.com>
2020-09-13 19:35:40 +0300

    Start d4

---
+++ src/main/java/gr/serafeim/Day4.java
@@ -0,0 +1,66 @@
+package gr.serafeim;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Day4 {
+    record Room(String name, int id, String checksum) {
+        public boolean isReal() {
+            Map<Character, Long> freqs = name.chars().mapToObj(i -> (char) i).filter(c -> c != '-').collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+            System.out.println(freqs);
+            return true;
+        }
+    }
+
+    static Room parseRoom(String s) {
+        String regex = "((\\D+)-(\\d+)\\[(\\D+)\\])";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+        matcher.find();
+
+        String g1= matcher.group(1);
+        String g2= matcher.group(2);
+        String g3= matcher.group(3);
+        String g4= matcher.group(4);
+        return new Room(g2, Integer.parseInt(g3), g4);
+
+    }
+
+    static ArrayList<Room> readInput() throws IOException {
+        final File file = new File("day4.txt");
+        final BufferedReader br = new BufferedReader(new FileReader(file));
+        String l;
+        ArrayList<Room> res = new ArrayList<>();
+
+        while ((l = br.readLine()) != null) {
+            res.add(parseRoom(l));
+        }
+
+        return res;
+    }
+
+
+
+    public static void part1() {
+        try {
+            ArrayList<Room> rooms = readInput();
+            //int res = Util.foldLeft(rooms.stream().filter(Room::isReal), 0, (acc, el) -> el.id + acc );
+            //System.out.println(res);
+            System.out.println(parseRoom("shoewudys-tou-ixyffydw-478[uszty]").isReal());
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
--- src/main/java/gr/serafeim/Main.java
+++ src/main/java/gr/serafeim/Main.java
@@ -3,7 +3,7 @@ package gr.serafeim;
 public class Main {
 
     public static void main(String[] args) {
-        Day3.part2();
+        Day4.part1();
         
 
     }
