3a9d28cd4fe2523423922b9e1851c6904869d34a
Serafeim Papastefanos <spapas@gmail.com>
2020-09-16 23:20:51 +0300

    Day 5 ok

--- src/main/java/gr/serafeim/Day5.java
+++ src/main/java/gr/serafeim/Day5.java
@@ -49,6 +49,36 @@ public class Day5 {
         }
     }
 
+    public static void part2() {
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            int cnt = 0;
+            HashMap<Integer, String> positions = new HashMap<>();
+            while(true) {
+                cnt++;
 
+                String s = "cxdnnyjw" + cnt;
+                byte[] digest = md5.digest(s.getBytes());
+                String ss = toHexString(digest);
+                if(ss.startsWith("00000")) {
+                    char pos = ss.charAt(5);
+                    String val = ""+ss.charAt(6);
+
+                    if (pos < '0' || pos > '7') continue;
+                    if (positions.containsKey(Integer.parseInt(""+pos))) continue;
+                    positions.put(Integer.parseInt(""+pos), val);
+                    if(positions.keySet().size()==8) break;
+                    System.out.println(positions);
+                }
+
+            }
+            System.out.println(positions);
+            for(int i=0;i<8;i++) {
+                System.out.print(positions.get(i));
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/gr/serafeim/Main.java
+++ src/main/java/gr/serafeim/Main.java
@@ -3,7 +3,7 @@ package gr.serafeim;
 public class Main {
 
     public static void main(String[] args) {
-        Day5.part1();
+        Day5.part2();
         
 
     }
